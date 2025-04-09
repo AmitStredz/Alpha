d4b623403df0050897fc03a1fb86805b82f4957f
AmitStredz <ponnan2112@gmail.com>
2025-04-09 14:21:39 +0530

    Update subscriptions.js

--- src/views/dashboard/pages/subscriptions.js
+++ src/views/dashboard/pages/subscriptions.js
@@ -1,43 +1,57 @@
-import React, { useState } from 'react'
-import PaymentModal from '../../../components/modals/paymentModal';
-import { IoIosArrowRoundForward } from 'react-icons/io';
-import { CiExport } from 'react-icons/ci';
+import React, { useState } from "react";
+import PaymentModal from "../../../components/modals/paymentModal";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import { CiExport } from "react-icons/ci";
 
 export default function Subscriptions() {
   const [isPaymentModal, setIsPaymentModal] = useState(false);
 
-    const cardList = [
-        {
-          title: "Life Time Plan",
-          primaryPrice: "$500.00",
-          secondaryPrice: "$600.00",
-          descriptionList: [
-            "Everything in Innovator, plus",
-            "Competitoe Benchmarking",
-            "Holistic Market Visualization",
-            "Adaptive Stategy Planner",
-            "24/7 Priority Support",
-          ],
-          planType: "1_year",
-        },
-        {
-          title: "1 Year Plan",
-          primaryPrice: "$200.00",
-          secondaryPrice: "$300.00",
-          descriptionList: [
-            "Everything in Innovator, plus",
-            "Competitoe Benchmarking",
-            "Holistic Market Visualization",
-            "Adaptive Stategy Planner",
-            "24/7 Priority Support",
-          ],
-          planType: "lifetime",
-        },
-      ];
+  const cardList = [
+    {
+      title: "Alpha Robotics Trading Bot",
+      plan: "One Year Plan",
+      primaryPrice: "₹15250.00",
+      secondaryPrice: "+ 18% GST",
+      secondaryPrice2: "₹2745.00",
+      totalInPaise: 1799500,
+      descriptionList: [
+        "Access to Alpha Trading Bot for 12 months",
+        "Smart AI-based trading strategies",
+        "Real-time market monitoring",
+        "Automatic trade execution",
+        "24/7 customer support",
+      ],
+    },
+    // {
+    //   title: "Life Time Plan",
+    //   primaryPrice: "$500.00",
+    //   secondaryPrice: "$600.00",
+    //   descriptionList: [
+    //     "Everything in Innovator, plus",
+    //     "Competitoe Benchmarking",
+    //     "Holistic Market Visualization",
+    //     "Adaptive Stategy Planner",
+    //     "24/7 Priority Support",
+    //   ],
+    //   planType: "1_year",
+    // },
+    // {
+    //   title: "1 Year Plan",
+    //   primaryPrice: "$200.00",
+    //   secondaryPrice: "$300.00",
+    //   descriptionList: [
+    //     "Everything in Innovator, plus",
+    //     "Competitoe Benchmarking",
+    //     "Holistic Market Visualization",
+    //     "Adaptive Stategy Planner",
+    //     "24/7 Priority Support",
+    //   ],
+    //   planType: "lifetime",
+    // },
+  ];
 
   return (
-
-<div className="flex flex-col gap-5 p-5 sm:p-10 w-full min-h-screen">
+    <div className="flex flex-col gap-5 p-5 sm:p-10 w-full min-h-screen">
       <div className="flex ">
         <span className="text-[38px] text-white">Billing & Subscriptions</span>
       </div>
@@ -62,53 +76,68 @@ export default function Subscriptions() {
       </div>
       </div> */}
 
-        {/* Pricing Cards */}
-                <div className="flex flex-col sm:flex-row gap-8 justify-center">
-                  {cardList?.map((items, index) => (
-                    <div
-                      key={index}
-                      className=" bg-white rounded-xl p-3 sm:p-5 sm:px-10 flex flex-col gap-3 items-center"
-                    >
-                      <div>
-                        <h3 className="text-xl font-sans font-400 text-gray-800 text-center">
-                          {items.title}
-                        </h3>
-                        <div className="flex items-end">
-                          <span className="text-3xl font-bold text-green-500">
-                            {items.primaryPrice}/
-                          </span>
-                          <span className="text-gray-500">{items.secondaryPrice}</span>
-                        </div>
-                      </div>
-                      <div className="w-full h-[1px] bg-slate-400"></div>
-                      <ul className="text-gray-700 space-y-2">
-                        {items.descriptionList?.map((desc, index) => (
-                          <li key={index}>✔ {desc}</li>
-                        ))}
-                      </ul>
-                      <div
-                        className={`flex justify-center items-center text-black py-2 px-4 w-full mt-10 rounded-lg shadow-md  cursor-pointer ${
-                          index === 0
-                            ? "bg-gradient-to-b from-[#1BAA4C] to-[#34CD69] text-white"
-                            : "border border-slate-400"
-                        }`}
-                        onClick={() => setIsPaymentModal(true)}
-                      >
-                        <span>Buy Now</span>
-                        <IoIosArrowRoundForward size={30} />
-                      </div>
-                    </div>
-                  ))}
+      {/* Pricing Cards */}
+      <div className="flex flex-col sm:flex-row justify-center gap-8 w-full">
+        {cardList?.map((items, index) => (
+          <div
+            key={index}
+            className=" bg-white rounded-xl p-3 sm:p-5 sm:px-10 flex flex-col gap-3 items-center sm:w-[25rem]"
+          >
+            <div>
+              <h3 className="text-xl font-400 text-gray-800 text-center">
+                {items.title}
+              </h3>
+              <h2 className="text-lg font-400 text-gray-800 text-center">
+                {items.plan}
+              </h2>
+              <div className="flex items-end">
+                <span className="text-3xl font-bold text-green-500">
+                  {items.primaryPrice}/
+                </span>
+                <div className="text-[14px] text-gray-500 text-center w-full flex flex-col flex-nowrap">
+                  <span>{items.secondaryPrice}</span>
+                  <span className="text-green-500">
+                    {items.secondaryPrice2}
+                  </span>
                 </div>
+              </div>
+            </div>
+            <div className="w-full h-[1px] bg-slate-400"></div>
+            <ul className="text-gray-700 space-y-2">
+              {items.descriptionList?.map((desc, index) => (
+                <li key={index} className="text-[14px] sm:text-[16px]">
+                  ✔ {desc}
+                </li>
+              ))}
+            </ul>
+            {/* <div
+                className={`flex justify-center items-center text-black py-2 px-4 w-full mt-10 rounded-lg shadow-md  cursor-pointer ${
+                  index === 0
+                    ? "bg-gradient-to-b from-[#1BAA4C] to-[#34CD69] text-white"
+                    : "border border-slate-400"
+                }`}
+                // onClick={handleBuyNow}
+              >
+                {loading ? (
+                  "Processing..."
+                ) : (
+                  <>
+                    <span>Buy Now</span>
+                    <IoIosArrowRoundForward size={30} />
+                  </>
+                )}
+              </div> */}
+          </div>
+        ))}
+      </div>
 
-       {/* Table */}
-       <div className="flex flex-col  bg-white rounded-2xl w-full">
+      {/* Table */}
+      {/* <div className="flex flex-col  bg-white rounded-2xl w-full">
         <div className="header flex justify-between items-center w-full bg-slate-200 p-5 rounded-t-2xl">
           <span>Billing History</span>
           <div className="flex gap-2 items-center border-black border p-2 rounded-lg">
             <span>Export</span>
             <CiExport />
-
           </div>
         </div>
 
@@ -124,7 +153,7 @@ export default function Subscriptions() {
                 <th className="">Action</th>
               </tr>
             </thead>
-            {/* {swapHistory.length > 0 ? (
+            {swapHistory.length > 0 ? (
               swapHistory?.map((item, index) => (
                 <tbody className="text-[12px] sm:text-[16px] font-200 z-[1000000]">
                   <tr key={index} className="border-b border-gray-700">
@@ -137,16 +166,13 @@ export default function Subscriptions() {
               ))
             ) : (
               <div>No swapping history found.</div>
-            )} */}
+            )}
           </table>
         </div>
-
-        {isPaymentModal && (
-                <PaymentModal onclose={() => setIsPaymentModal(false)} />
-              )}
-
-      </div>
-
-
-    </div>)
+      </div> */}
+      {isPaymentModal && (
+        <PaymentModal onclose={() => setIsPaymentModal(false)} />
+      )}
+    </div>
+  );
 }
